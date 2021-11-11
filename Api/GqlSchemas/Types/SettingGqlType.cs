0e43dba9dfc386f15c44c473d624307796745c81
Lulus <snys98@outlook.com>
2021-11-11 14:35:47 +0800

    重新提取gql模块 geexDbcontext优化Migration获取方式 重新采用objectDescriptor, 取消默认的接口descriptor方式

--- Api/GqlSchemas/Types/SettingGqlType.cs
+++ Api/GqlSchemas/Types/SettingGqlType.cs
@@ -1,14 +1,21 @@
 ﻿using Geex.Common.Settings.Api.Aggregates.Settings;
+using Geex.Common.Settings.Core;
 using HotChocolate.Types;
 
 namespace Geex.Common.Settings.Api.GqlSchemas.Types
 {
-    public class SettingGqlType : ObjectType<ISetting>
+    public class SettingGqlType : ObjectType<Setting>
     {
-        protected override void Configure(IObjectTypeDescriptor<ISetting> descriptor)
+        protected override void Configure(IObjectTypeDescriptor<Setting> descriptor)
         {
-            descriptor.BindFieldsImplicitly();
+            descriptor.BindFieldsExplicitly();
+            descriptor.Implements<InterfaceType<ISetting>>();
+            descriptor.ConfigEntity();
             descriptor.Field(x => x.Scope);
+            descriptor.Field(x => x.ScopedKey);
+            descriptor.Field(x => x.Name);
+            descriptor.Field(x => x.Id);
+            descriptor.Field(x => x.Value);
             base.Configure(descriptor);
         }
     }
